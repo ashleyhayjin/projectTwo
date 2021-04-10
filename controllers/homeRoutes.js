b9af89a455149fc2bb6be691d380e36de897bba0
Michael Jones <mtjones1979@yahoo.com>
2021-04-10 12:40:33 -0500

    updated dashboard

--- controllers/homeRoutes.js
+++ controllers/homeRoutes.js
@@ -18,7 +18,7 @@ router.get('/', async (req, res) => {
     const projects = projectData.map((project) => project.get({ plain: true }));
 
     // Pass serialized data and session flag into template
-    res.render('homepage', { 
+    res.render('/dashboard', { 
       projects, 
       logged_in: req.session.logged_in 
     });
@@ -31,7 +31,7 @@ router.get('/', async (req, res) => {
 router.get('/login', (req, res) => {
   // If the user is already logged in, redirect the request to another route
   if (req.session.logged_in) {
-    res.redirect('/profile');
+    res.redirect('/dashboard');
     return;
   }
 
