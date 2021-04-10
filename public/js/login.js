e295a77721afe50a9db634ebb89313a9a985e6ef
Jaxon D <duncan5irjax@gmail.com>
2021-04-10 12:50:52 -0500

    Fixed the login page not redirecting and always saying incorrect password

--- public/js/login.js
+++ public/js/login.js
@@ -1,27 +1,28 @@
 const loginFormHandler = async (event) => {
-    event.preventDefault();
+  event.preventDefault();
+
+  // Collect values from the login form
+  const username = document.querySelector('#username-login').value.trim();
+  const password = document.querySelector('#password-login').value.trim();
   
-    // Collect values from the login form
-    const username = document.querySelector('#username-login').value.trim();
-    const password = document.querySelector('#password-login').value.trim();
-  
-    if (username && password) {
-      // Send a POST request to the API endpoint
-      const response = await fetch('/api/users/login', {
-        method: 'POST',
-        body: JSON.stringify({ username, password }),
-        headers: { 'Content-Type': 'application/json' },
-      });
-  
-      if (response.ok) {
-        // If successful, redirect the browser to the profile page
-        document.location.replace('/dashboard');
-      } else {
-        alert("Failed to log in");
-      }
+  if (username && password) {
+    // Send a POST request to the API endpoint
+    const response = await fetch('/api/users/login', {
+      method: 'POST',
+      body: JSON.stringify({ username, password }),
+      headers: { 'Content-Type': 'application/json' },
+    });
+
+    if (response.ok) {
+      // If successful, redirect the browser to the profile page
+      document.location.replace('/dashboard');
+    } else {
+      alert("Failed to log in");
     }
-  };
-  document.querySelector('#login-form').addEventListener('submit', loginFormHandler);
+  }
+};
+
+document.querySelector('#login-form').addEventListener('submit', loginFormHandler);
   
   // const signupFormHandler = async (event) => {
   //   event.preventDefault();
