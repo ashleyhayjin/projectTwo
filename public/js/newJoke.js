4a3452fa521547eec16d939d2419671c0d6640b7
Ashley Lee <ashleyhayjin@iMac.fios-router.home>
2021-04-12 19:41:50 -0500

    Joke Routes

--- public/js/newJoke.js
+++ public/js/newJoke.js
@@ -1,17 +1,20 @@
-// const newFormHandler = async (event) => {
-//     event.preventDefault();
+const newFormHandler = async (event) => {
+    event.preventDefault();
   
-//     const joke_text = document.querySelector('#joke-content').value.trim();
-//     console.log(joke_text);
-//    await fetch(`/api/jokes`, {
-//         method: 'POST',
-//         body: JSON.stringify({ joke_text }),
-//         headers: {
-//           'Content-Type': 'application/json',
-//         },
-//       });
-  
-//     document.location.replace('/');
-//     };
-//   document.querySelector('.new-joke-form').addEventListener('submit', newFormHandler);
+    const joke_text = document.querySelector('#joke-content').value.trim();
+    
+    if(joke_text){
+        const response = await fetch(`/api/jokes`, {
+            method: 'POST',
+            body: JSON.stringify({ joke_text }),
+            headers: {'Content-Type': 'application/json',},
+      });
+      if (response.ok)  {
+         document.location.replace('/');
+        } else {
+            alert(response.statusText);
+        }
+    }
+};
+document.querySelector('#new-joke-form').addEventListener('submit', newFormHandler);
   
