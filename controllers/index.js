57e9f45f37e76695705cef2a105dc3d79490f6d7
Jaxon D <duncan5irjax@gmail.com>
2021-04-14 19:54:40 -0500

    Another attempt at fixing the redirect bug

--- controllers/index.js
+++ controllers/index.js
@@ -2,8 +2,10 @@ const router = require('express').Router();
 
 const apiRoutes = require('./api')
 const homeRoutes = require('./homeRoutes');
+const postto = require('../postto');
 
 router.use('/api', apiRoutes);
+router.use('/post', postto);
 router.use('/', homeRoutes);
 
 module.exports = router;
